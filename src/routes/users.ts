40680a1ff902fe31be8daa587ea2f058734f2562
TomOwen93 <mr.tsowen@gmail.com>
2023-09-12 15:59:22 +0100

    change discord webhook package

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,14 +1,13 @@
 import express, { Router } from "express";
 import { Client } from "pg";
-import { EmbedBuilder, WebhookClient } from "discord.js";
+import { Webhook, MessageBuilder } from "discord-webhook-node";
 
 export default function createUsersRouter(client: Client): Router {
     const router = express.Router();
 
-    const webhookClient = new WebhookClient({
-        id: "1151156877011451944",
-        token: "Bsd58OG6UGFlIiSLSiIrh4DKiEHZu8txjdQssHWjLhtfNlCvo2u5Jllu66V1ybXhVU9Z",
-    });
+    const hook = new Webhook(
+        "https://discord.com/api/webhooks/1151156877011451944/Bsd58OG6UGFlIiSLSiIrh4DKiEHZu8txjdQssHWjLhtfNlCvo2u5Jllu66V1ybXhVU9Z"
+    );
 
     router.get("/", async (_req, res) => {
         try {
@@ -29,16 +28,13 @@ export default function createUsersRouter(client: Client): Router {
             const response = await client.query(text, values);
             res.status(200).json(response.rows);
 
-            const embed = new EmbedBuilder()
+            const embed = new MessageBuilder()
                 .setTitle("New User Added!")
-                .setColor(0x00ffff);
+                .setColor(0x00ffff)
+                .setAuthor(user_name)
+                .setDescription(JSON.stringify(response.rows));
 
-            webhookClient.send({
-                content: JSON.stringify(response.rows),
-                username: user_name,
-                avatarURL: "https://i.imgur.com/AfFp7pu.png",
-                embeds: [embed],
-            });
+            hook.send(embed);
         } catch (err) {
             console.error(err);
             res.status(500).json({ error: "Internal server error" });

bb819d07b9ccf6f2980ed35bee74d30696972f95
dani voro <mr.voro@gmail.com>
2023-09-15 12:15:00 +0200

    addign get resources by id

--- src/routes/resources.ts
+++ src/routes/resources.ts
@@ -61,5 +61,18 @@ export default function createResourcesRouter(client: Client): Router {
         }
     });
 
+    router.get("/:id", async (req, res) => {
+        try {
+            const { id } = req.params;
+            const text = "SELECT * FROM resources WHERE resource_id = $1";
+            const values = [id];
+            const result = await client.query(text, values);
+            res.status(200).json(result.rows);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: "Internal server error" });
+        }
+    });
+
     return router;
 }

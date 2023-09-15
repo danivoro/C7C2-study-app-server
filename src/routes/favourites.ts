3522e6ce9ebd630179406fc45646894230c94ed7
dani voro <mr.voro@gmail.com>
2023-09-15 16:40:19 +0200

    adding put for favourites

--- src/routes/favourites.ts
+++ src/routes/favourites.ts
@@ -28,5 +28,19 @@ export default function createFavouritesRouter(client: Client): Router {
         }
     });
 
+    router.post("/", async (req, res) => {
+        try {
+            const { resource_id, user_id } = req.body;
+            const text =
+                "INSERT INTO favourites (resource_id, user_id) VALUES($1, $2)";
+            const values = [resource_id, user_id];
+            const response = await client.query(text, values);
+            res.status(200).json(response.rows);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: "Internal server error" });
+        }
+    });
+
     return router;
 }

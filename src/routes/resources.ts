8a646cc2c4f38f62ec2ff5eaeeb62f3641326174
dani voro <mr.voro@gmail.com>
2023-09-15 12:24:19 +0200

    put for resources like created

--- src/routes/resources.ts
+++ src/routes/resources.ts
@@ -74,5 +74,19 @@ export default function createResourcesRouter(client: Client): Router {
         }
     });
 
+    router.put("/:id", async (req, res) => {
+        try {
+            const { id } = req.params;
+            const text =
+                "UPDATE resources SET likes = likes + 1 WHERE resource_id = $1";
+            const values = [id];
+            const result = await client.query(text, values);
+            res.status(200).json(result.rows);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: "Internal server error" });
+        }
+    });
+
     return router;
 }

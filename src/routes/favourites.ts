8d9ff14d033a4a762f918268a12ee811d3f15914
dani voro <mr.voro@gmail.com>
2023-09-19 19:15:04 +0200

    preventing duplicate favourites

--- src/routes/favourites.ts
+++ src/routes/favourites.ts
@@ -49,11 +49,24 @@ export default function createFavouritesRouter(client: Client): Router {
     router.post("/", async (req, res) => {
         try {
             const { resource_id, user_id } = req.body;
-            const text =
-                "INSERT INTO favourites (resource_id, user_id) VALUES($1, $2)";
-            const values = [resource_id, user_id];
-            const response = await client.query(text, values);
-            res.status(200).json(response.rows);
+
+            const checkQuery =
+                "SELECT * FROM favourites WHERE resource_id = $1 AND user_id = $2";
+            const checkValues = [resource_id, user_id];
+            const checkResult = await client.query(checkQuery, checkValues);
+
+            if (checkResult.rows.length === 0) {
+                const insertQuery =
+                    "INSERT INTO favourites (resource_id, user_id) VALUES($1, $2)";
+                const insertValues = [resource_id, user_id];
+                const response = await client.query(insertQuery, insertValues);
+
+                res.status(200).json(response.rows);
+            } else {
+                res.status(400).json({
+                    message: "You already have this in your favourites.",
+                });
+            }
         } catch (err) {
             console.error(err);
             res.status(500).json({ error: "Internal server error" });
